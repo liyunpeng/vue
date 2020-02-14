eac002960732565719712038baee983ca52f70cd
lijinyang <kylin27@126.com>
2020-02-14 20:59:41 +0800

    添加slot render用法

---
+++ transferdata/child.vue
@@ -0,0 +1,18 @@
+<!-- 子组件 -->
+<template>
+    <div>
+        <span>{{ parentMessage }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            parentMessage: {
+                type: String,
+                default: '默认显示的信息'
+                // require: true // 必填
+            }
+        }
+    }
+</script>
---
+++ transferdata/parent.vue
@@ -0,0 +1,21 @@
+<!-- 父组件 -->
+<template>
+    <div>
+        <my-child :parentMessage="parentMessage"></my-child>
+    </div>
+</template>
+
+<script>
+    import MyChild from '@components/common/MyChild'
+
+    export default {
+        components: {
+            MyChild
+        },
+        data() {
+            return {
+                parentMessage: "我是来自父组件的消息"
+            }
+        }
+    }
+</script>
